778d3c9edc13d6439881b7662a3db0a11df77fd9
CCN2 <CCN2@ucd.ac.ma>
2025-05-19 16:24:24 +0000

    Initial commit: Docker + backend + db setup

--- app/index.js
+++ app/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+
+// Utiliser le port fourni par l'environnement Docker ou 8080 par défaut
+const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.send('Hello Houda');
 });
 
-app.listen(PORT, () => {
-  console.log('Serveur démarré sur http://localhost:${PORT}');
+// Très important : écouter sur 0.0.0.0 pour que Docker puisse exposer le port
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Serveur démarré sur http://localhost:${PORT}`);
 });
